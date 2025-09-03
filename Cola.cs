c467014a3d96d4825673d2ce9a29cfa2401edec0
Cesar-txt <utp0153205@alumno.utpuebla.edu.mx>
2025-09-03 00:23:09 -0600

    Add files via upload

---
+++ Cola.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cola
+{
+    internal class Cola
+    {
+        private int[] elementos;
+        private int frente;
+        private int final;
+        private int contador;
+        private int max;
+
+
+        public Cola(int tamaño)
+        {
+            max = tamaño;
+            elementos = new int[max];
+            frente = 0;
+            final = -1;
+            contador = 0;
+        }
+
+        public bool Enqueue(int dato)
+        {
+            if (contador < max)
+            {
+                final = (final + 1) % max;
+                elementos[final] = dato;
+                contador++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int Dequeue()
+        {
+            if (contador > 0)
+            {
+                int elemento = elementos[frente];
+                frente = (frente + 1) % max;
+                contador--;
+                return elemento;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public int Peek()
+        {
+            return (contador > 0) ? elementos[frente] : -1;
+        }
+
+        public bool EstaVacia()
+        {
+            return contador == 0;
+        }
+
+        public bool EstaLlena()
+        {
+            return contador == max;
+        }
+
+        public int Tamaño()
+        {
+            return contador;
+        }
+
+        public int ObtenerFrente()
+        {
+            return frente;
+        }
+
+        public int ObtenerMax()
+        {
+            return max;
+        }
+
+        public int ObtenerElemento(int indice)
+        {
+            return elementos[indice];
+        }
+
+        public string MostrarCola()
+        {
+            if (EstaVacia())
+                return "Cola vacía";
+
+            string resultado = "Cola: [";
+            for (int i = 0; i < contador; i++)
+            {
+                int indice = (frente + i) % max;
+                resultado += elementos[indice];
+                if (i < contador - 1)
+                    resultado += ", ";
+            }
+            resultado += "]";
+            return resultado;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cola
+{
+    public partial class Form1 : Form
+    {
+        private Cola cola;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cola = new Cola(5);
+
+        }
+        private void ActualizarVisualizacion()
+        {
+            listBox1.Items.Clear();
+
+            if (cola.EstaVacia())
+            {
+                listBox1.Items.Add("Cola vacía");
+            }
+            else
+            {
+
+                for (int i = 0; i < cola.Tamaño(); i++)
+                {
+                    int indice = (cola.ObtenerFrente() + i) % cola.ObtenerMax();
+                    int elemento = cola.ObtenerElemento(indice);
+
+                    if (i == 0)
+                        listBox1.Items.Add($"{elemento}");
+                    else
+                        listBox1.Items.Add($"{elemento}");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int tamaño = int.Parse(textBox1.Text);
+                if (tamaño > 0)
+                {
+                    cola = new Cola(tamaño);
+                    label4.Text = $"Nueva cola creada con tamaño {tamaño}";
+                    ActualizarVisualizacion();
+                }
+                else
+                {
+                    label4.Text = "El tamaño debe ser mayor a 0";
+                }
+            }
+            catch (FormatException)
+            {
+                label4.Text = "Por favor ingrese un número válido para el tamaño";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int elemento = int.Parse(textBox2.Text);
+                if (cola.Enqueue(elemento))
+                {
+                    label4.Text = $"Elemento {elemento} encolado exitosamente";
+                    textBox2.Clear();
+                }
+                else
+                {
+                    label4.Text = "La cola está llena";
+                }
+                ActualizarVisualizacion();
+            }
+            catch (FormatException)
+            {
+                label4.Text = "Por favor ingrese un número válido";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int elemento = cola.Dequeue();
+            if (elemento != -1)
+            {
+                label4.Text = $"Elemento desencolado: {elemento}";
+            }
+            else
+            {
+                label4.Text = "La cola está vacía";
+            }
+            ActualizarVisualizacion();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int elemento = cola.Peek();
+            if (elemento != -1)
+            {
+                label4.Text = $"Primer elemento (Peek): {elemento}";
+            }
+            else
+            {
+                label4.Text = "La cola está vacía";
+            }
+        }
+    }
+    
+}
